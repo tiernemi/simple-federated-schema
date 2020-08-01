9e5833aa824a913b1c116c0dba9642f8a7ee18e5
tiernemi <tiernemi@tcd.ie>
2020-08-02 00:29:20 +0100

    Cleanup

--- serviceA/src/main/kotlin/com/federation/spike/serviceA/ServiceA.kt
+++ serviceA/src/main/kotlin/com/federation/spike/serviceA/ServiceA.kt
@@ -1,14 +1,7 @@
 package com.federation.spike.serviceA
 
-import com.expediagroup.graphql.TopLevelObject
-import com.expediagroup.graphql.federation.FederatedSchemaGeneratorConfig
-import com.expediagroup.graphql.federation.FederatedSchemaGeneratorHooks
-import com.expediagroup.graphql.federation.execution.FederatedTypeRegistry
-import com.expediagroup.graphql.federation.toFederatedSchema
-import graphql.schema.GraphQLSchema
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class ServiceA
